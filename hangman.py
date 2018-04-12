891374ae12e1ba3dabc19578169151a414637c0b
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-04-12 19:25:28 +0100

    ip config changed

--- hangman.py
+++ hangman.py
@@ -408,7 +408,10 @@ def guess(username, guess_data):
     
 
 
-if __name__ == "__main__":
-    app.run(host=os.getenv("IP"),port=os.getenv("PORT"), debug=True)
+##if __name__ == "__main__":
+    ##app.run(host=os.getenv("IP"),port=os.getenv("PORT"), debug=True)
 
+port = int(os.environ.get("PORT",5000))
 
+if __name__ == '__main__':
+    app.run(debug=False, host="0.0.0.0", port = port)

7656bac299dacab31807297ae38b79cdb6f2d29d
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-07-03 14:55:15 +0100

    local bootstrap.css

--- hangman.py
+++ hangman.py
@@ -63,10 +63,9 @@ if __name__ == "__main__":
     app.run(host=os.getenv("IP"),port=os.getenv("PORT"), debug=True)
 """
 
-port = int(os.environ.get("PORT",5000))
-##host = int(os.environ.get("IP",0.0.0.0))
+PORT = int(os.environ.get("PORT",5000))
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port = port)
+    app.run(debug=False, host="0.0.0.0", port=PORT)
 
 
